2b69f20ebdfda2b5dc4c86896784776a8535440f
Owais Khan <160714084+Owais-Khan86@users.noreply.github.com>
2024-10-15 23:18:35 +0500

    Add files via upload

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>2024 Next.js Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Assignment - 02</h1>
+      <h2>Next.js Website</h2>
+    </header>
+  );
+};
+
+export default Header;
